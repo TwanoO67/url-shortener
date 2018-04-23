d7aba213c69b7296cd902c5f947efa29a8731ea6
Antoine WEBER <a.weber@sdv.fr>
2018-04-23 15:09:32 +0200

    ajout de random

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
 	//template from https://codepen.io/nikhil/pen/GuAho
+	error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 	
-	//CONFIGURATION
-	$titre = "Les Photos de";
-	$domain = "http://les-photos.de";
-	$filename = "links.json";
-	
-	$protected = false;
-	$password="AChangerPourActiverLeModeProtected";
+	require_once("config.php");
 	
 	//variable intermediaire
 	$lien = "";
@@ -24,12 +19,12 @@
 	//mode d'ajout
 	if($_POST['mode'] == 'add'){
 		if( !$protected || $_POST['pwd'] === $password ){
-			if( !isset( $links[$_POST['shortlink']]) ){
+			if( !isset( $links[$_POST['shortlink']]) && $_POST['shortlink'] !== '/' ){
 				//ajout du lien
 				$links[$_POST['shortlink']] = $_POST['link'];
 				//sauvegarde
 				file_put_contents($filename, json_encode($links, JSON_PRETTY_PRINT));
-				$message = " Ok, votre lien a été créer <a href='"$domain.$_POST['shortlink']."'>"$domain.$_POST['shortlink']."</a> ";			}
+				$message = " Ok, votre lien a été créer <a href='".$domain.$_POST['shortlink']."'>".$domain.$_POST['shortlink']."</a> ";			}
 			else{
 				$message = " Erreur, Le lien existe déjà ! ";
 			}
@@ -39,162 +34,39 @@
 		}
 		
 	}
+	
+	
+	function getUniqueId(){
+		require_once("uniqueID.php");
+
+		// Create an instance of uniqueID
+		$uniqueID = new uniqueID();
+		
+		// Generate an ID
+		$id = $uniqueID->generate();
+		//echo $id; // 2398161031202658563
+		
+		// Shorten an ID
+		$shortId = $uniqueID->shorten($id);
+		return $shortId;
+	}
+	
+	
+	
+	
 ?>
 
 <html lang="fr">
 	<head>
-		<meta charset="iso-8859-1">
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="icon" type="image/png" href="camera.png" />
 		<title><?php echo $titre; ?></title>
 		<?php if ( $lien !== "") { ?>
 		<meta http-equiv="refresh" content="0; URL=<?php echo $lien; ?>">
 		<?php } ?>
 	</head>
 	<body>
-		<style>
-			 body {
-		        background: #555 url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAB9JREFUeNpi/P//PwM6YGLAAuCCmpqacC2MRGsHCDAA+fIHfeQbO8kAAAAASUVORK5CYII=);
-		        font: 13px 'Lucida sans', Arial, Helvetica;
-		        color: #eee;
-		        text-align: center;
-		    }
-		    
-		    a {
-		        color: #ccc;
-		    }
-		    
-		    /*-------------------------------------*/
-		    
-		    .cf:before, .cf:after{
-		      content:"";
-		      display:table;
-		    }
-		    
-		    .cf:after{
-		      clear:both;
-		    }
-		
-		    .cf{
-		      zoom:1;
-		    }
-		
-		    /*-------------------------------------*/	
-		    
-		    .form-wrapper {
-		        width: 450px;
-		        padding: 15px;
-		        margin: 150px auto 50px auto;
-		        background: #444;
-		        background: rgba(0,0,0,.2);
-		        -moz-border-radius: 10px;
-		        -webkit-border-radius: 10px;
-		        border-radius: 10px;
-		        -moz-box-shadow: 0 1px 1px rgba(0,0,0,.4) inset, 0 1px 0 rgba(255,255,255,.2);
-		        -webkit-box-shadow: 0 1px 1px rgba(0,0,0,.4) inset, 0 1px 0 rgba(255,255,255,.2);
-		        box-shadow: 0 1px 1px rgba(0,0,0,.4) inset, 0 1px 0 rgba(255,255,255,.2);
-		    }
-		    
-		    .form-wrapper input {
-		        width: 330px;
-		        height: 40px;
-		        padding: 10px 5px;
-		        margin-bottom: 5px;
-		        float: left;    
-		        font: bold 15px 'lucida sans', 'trebuchet MS', 'Tahoma';
-		        border: 0;
-		        background: #eee;
-		        -moz-border-radius: 3px 0 0 3px;
-		        -webkit-border-radius: 3px 0 0 3px;
-		        border-radius: 3px 0 0 3px;      
-		    }
-		    
-		    .form-wrapper input:focus {
-		        outline: 0;
-		        background: #fff;
-		        -moz-box-shadow: 0 0 2px rgba(0,0,0,.8) inset;
-		        -webkit-box-shadow: 0 0 2px rgba(0,0,0,.8) inset;
-		        box-shadow: 0 0 2px rgba(0,0,0,.8) inset;
-		    }
-		    
-		    .form-wrapper input::-webkit-input-placeholder {
-		       color: #999;
-		       font-weight: normal;
-		       font-style: italic;
-		    }
-		    
-		    .form-wrapper input:-moz-placeholder {
-		        color: #999;
-		        font-weight: normal;
-		        font-style: italic;
-		    }
-		    
-		    .form-wrapper input:-ms-input-placeholder {
-		        color: #999;
-		        font-weight: normal;
-		        font-style: italic;
-		    }    
-		    
-		    .form-wrapper button {
-				overflow: visible;
-		        position: relative;
-		        float: right;
-		        border: 0;
-		        padding: 0;
-		        cursor: pointer;
-		        height: 40px;
-		        width: 110px;
-		        font: bold 15px/40px 'lucida sans', 'trebuchet MS', 'Tahoma';
-		        color: #fff;
-		        text-transform: uppercase;
-		        background: #d83c3c;
-		        -moz-border-radius: 0 3px 3px 0;
-		        -webkit-border-radius: 0 3px 3px 0;
-		        border-radius: 0 3px 3px 0;      
-		        text-shadow: 0 -1px 0 rgba(0, 0 ,0, .3);
-		    }   
-		      
-		    .form-wrapper button:hover{		
-		        background: #e54040;
-		    }	
-		      
-		    .form-wrapper button:active,
-		    .form-wrapper button:focus{   
-		        background: #c42f2f;    
-		    }
-		    
-		    .form-wrapper button:before {
-		        content: '';
-		        position: absolute;
-		        border-width: 8px 8px 8px 0;
-		        border-style: solid solid solid none;
-		        border-color: transparent #d83c3c transparent;
-		        top: 12px;
-		        left: -6px;
-		    }
-		    
-		    .form-wrapper button:hover:before{
-		        border-right-color: #e54040;
-		    }
-		    
-		    .form-wrapper button:focus:before{
-		        border-right-color: #c42f2f;
-		    }    
-		    
-		    .form-wrapper button::-moz-focus-inner {
-		        border: 0;
-		        padding: 0;
-		    }
-		.byline p{
-		  text-align:center;
-		  color:#c6c6c6;
-		  font: bold 18px Arial, Helvetica, Sans-serif;
-		  text-shadow: 0 2px 3px rgba(0,0,0,0.1);
-		}
-		
-		.byline p a{
-		  color:#d83c3c;
-		  text-decoration:none;
-		}
-		</style>
 		<br/><br/>
 		<h1> <?php echo $titre; ?> </h1>
 		<?php if ( $message !== '' ){
@@ -203,17 +75,27 @@
 		<form class="form-wrapper cf">
 			<p>Redirection vers <a href="<?php echo $lien; ?>"><?php echo $lien; ?></a> ...</p>
 		</form>
-		<?php } elseif ( $lien !== "/") { ?>
+		<?php } elseif ( $lien !== "/" ) { ?>
 			
 			
 			<form class="form-wrapper cf" action="/" method="post">
 				<p> 
-					Aucun lien existant pour <?php echo $shortlink; ?>.<br/> 
-					Voulez-vous en créer un ? 
+					<?php 
+					//cas particulier pour la home
+					if( $shortlink === "/" ){ 
+						echo "Voulez-vous créer un lien réduit ? <br/>";
+						$shortlink = getUniqueId(); 
+					}
+					else{
+						echo "Aucun lien existant pour ".$shortlink."<br/> ";
+						echo "Voulez-vous en créer un ? <br/>";
+					}
+					?>
 				</p>
 				<?php if($protected){ ?>
 				<input type="password" placeholder="Mot de passe" name="pwd" required><br/>
 				<?php } ?>
+				 
 				<input type='text' name="shortlink" value='<?php echo $shortlink; ?>'></input><br/>
 				<input type='hidden' name="mode" value='add'></input>
 		  	    <input type="text" placeholder="Lien complet... http://..." name="link" required>
